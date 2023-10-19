0e921c4e35c48a327617f4546a90ca25b98dcae9
Christian Crites <ccrites98@gmail.com>
2023-10-19 09:50:17 -0400

    first

--- src/main/java/com/pluralsight/SearchInventory.java
+++ src/main/java/com/pluralsight/SearchInventory.java
@@ -1,38 +1,52 @@
 package com.pluralsight;
 import java.util.*;
+import java.io.*;
+
+
 public class SearchInventory {
-//Once established, set up the main
+
+//Pull up that main function jamie
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
         ArrayList<Product> inventory = getInventory();
-        for (Product product : inventory) {
-            System.out.println(product);
-
+        System.out.println("What's in stock: ");
+        for (int i = 0; i < inventory.size(); i++) {
+            Product x = inventory.get(i);
+            System.out.printf("\n ID: %d| Name: %s| Price: $%.2f".formatted(x.getId(), x.getName(), x.getPrice()));
         }
     }
-    //Everybody loves Array-mond
-    public static ArrayList<Product> getInventory() {
-        ArrayList<Product> inventory = new ArrayList<>();
-        inventory.add(new Product("Hammer", 19.49, 50));
-        inventory.add(new Product("Box of Nails", 9.29, 30));
-        inventory.add(new Product("Mechanics Tool Set", 119.00, 20));
-        inventory.add(new Product("10' 2x4 (grade B)", 9.99, 40));
-        inventory.add(new Product("9-in-1 Ratcheting Screwdriver", 24.97, 15));
+    private static ArrayList<Product> getInventory() {
+        ArrayList<Product> inventory = new ArrayList<Product>();
+        inventory.add(new Product(1, "Uranium", 1.00F));
+        inventory.add(new Product(2, "A really cool sword", 9.99F));
+        inventory.add(new Product(3, "The love and respect of your father", 99999999999999999999999999999.99F));
+        inventory.add(new Product(4, "Ed Gein's Guide to Interior Decoration", 18.99F));
+        inventory.add(new Product(5, "Empanadas", 3.99F));
         return inventory;
     }
-//Set up the variables Jamie
+
+//Set up the product class, Jamie
 }
-class Product {
-    private String name;
-    private double price;
-    private int quantity;
-    public Product(String name, double price, int quantity) {
-        this.name = name;
-        this.price = price;
-        this.quantity = quantity;
+    class Product {
+        private int id =0; private String name = ""; private double price = 0.00;
+
+        public Product(int id, String name, double price) {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+
     }
 
-    public String toString() {
-        return "Product: " + name + " | Price: $" + price + " | Quantity: " + quantity;
-    }
-}
